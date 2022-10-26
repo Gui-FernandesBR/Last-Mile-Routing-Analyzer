2d00101eba305a9ca0ce001fb2966c6bdb06a26c
Gui-FernandesBR <guilherme_fernandes@usp.br>
2022-10-26 22:01:42 +0200

    ENH: convert utilities classes to functions

--- lmr_analyzer/utilities.py
+++ lmr_analyzer/utilities.py
@@ -1,33 +1,50 @@
+__author__ = "Guilherme Fernandes Alves"
 
 from math import radians, cos, sin, asin, sqrt
+import osmnx as ox
+import networkx as nx
+import googlemaps
 
-class distances:
+# Distances calculations
 
-    def __init__(self):
 
-        return None
-
-    def Haversine(lat1, lon1, lat2, lon2):
-        """
+def Haversine(lat1, lon1, lat2, lon2):
+    """
+    Calculate the great circle distance between two points
         Calculate the great circle distance between two points 
-        on the earth (specified in decimal degrees)
-        """
+    Calculate the great circle distance between two points
+    on the earth (specified in decimal degrees)
+    """
+    # convert decimal degrees to radians
         # convert decimal degrees to radians 
-        lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+    # convert decimal degrees to radians
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+    # haversine formula
         # haversine formula 
+    # haversine formula
+    dlon = lon2 - lon1
         dlon = lon2 - lon1 
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
         dlat = lat2 - lat1 
-        a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    dlat = lat2 - lat1
+    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+    c = 2 * asin(sqrt(a))
         c = 2 * asin(sqrt(a)) 
-        # Radius of earth in kilometers is 6371
-        km = 6371* c
-        return km
+    c = 2 * asin(sqrt(a))
+    # Radius of earth in kilometers is 6371
+    km = 6371 * c
+    return km
 
-    def drive_distance_gmaps(origin, destination):
-        import googlemaps
-        gmaps = googlemaps.Client(key='AIzaSyC9B4JfZMhjWd4v4q4kz1G6eY0l0j8KfYU') # TODO: parametrize key
-        directions_result = gmaps.directions(origin, destination, mode="driving")
-        return directions_result[0]['legs'][0]['distance']['value']
+
+def drive_distance_gmaps(origin, destination):
+
+    # TODO: parametrize key
+    gmaps = googlemaps.Client(key="AIzaSyC9B4JfZMhjWd4v4q4kz1G6eY0l0j8KfYU")
+    directions_result = gmaps.directions(origin, destination, mode="driving")
+    return directions_result[0]["legs"][0]["distance"]["value"]
+
+
 
     # def drive_distance_osrm(origin, destination):
     #     import requests
@@ -40,39 +57,40 @@ class distances:
     #     data = json.loads(response.text)
     #     return data['routes'][0]['legs'][0]['distance']
 
-    def drive_distance_osm(origin, destination):
-        pass
 
-    def drive_distance_bing(origin, destination):
-        pass
+def drive_distance_bing(origin, destination):
+    pass
 
-    def get_distance(location1, location2, mode='haversine'):
-        """_summary_
 
-        Parameters
-        ----------
-        location1 : _type_
-            _description_
-        location2 : _type_
-            _description_
-        mode : string
-            Distance calculation mode. The mode must be one of the following:
-            'haversine', 'gmaps', 'osm', 'bing'.
+def get_distance(location1, location2, mode="haversine"):
+    """_summary_
 
-        Returns
-        -------
-        _type_
-            _description_
+    Parameters
+    ----------
+    location1 : _type_
+        _description_
+    location2 : _type_
+        _description_
+    mode : string
+        Distance calculation mode. The mode must be one of the following:
+        'haversine', 'gmaps', 'osmnx', 'bing'.
+
+    Returns
+    -------
+    _type_
+        _description_
+    """
         """    
-        if mode == 'haversine':
-            return Haversine(location1[0], location1[1], location2[0], location2[1])
-        elif mode == 'gmaps':
-            return drive_distance_gmaps(location1, location2)
-        # elif mode == 'osrm':
-        #     return drive_distance_osrm(location1, location2)
-        elif mode == 'osm':
-            return drive_distance_osm(location1, location2)
-        elif mode == 'bing':
-            return drive_distance_bing(location1, location2)
-        else:
-            raise ValueError('Invalid mode, please choose one of the following: haversine, gmaps, osm, bing')
+    """
+    if mode == "haversine":
+        return Haversine(location1[0], location1[1], location2[0], location2[1])
+    elif mode == "gmaps":
+        return drive_distance_gmaps(location1, location2)
+    elif mode == "osmnx":
+        return drive_distance_osmnx(location1, location2)
+    elif mode == "bing":
+        return drive_distance_bing(location1, location2)
+    else:
+        raise ValueError(
+            "Invalid mode, please choose one of the following: haversine, gmaps, osmnx, bing"
+        )
