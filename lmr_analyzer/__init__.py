0f0319e1eeaddf713bbd867e6b610f23ec38a0ec
Gui-FernandesBR <guilherme_fernandes@usp.br>
2022-10-26 10:54:40 +0200

    FIX: creating __init__ module

---
+++ lmr_analyzer/__init__.py
@@ -0,0 +1,8 @@
+from .analysis import analysis
+
+# from .geometry import geometry
+from .package import package
+from .route import route
+from .stop import stop
+from .utilities import *
+from .vehicle import vehicle
