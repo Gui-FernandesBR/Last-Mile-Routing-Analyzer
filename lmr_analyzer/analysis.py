8b6877b9c9e2da25545573f26a81a101aa21809b
Gui-FernandesBR <guilherme_fernandes@usp.br>
2022-10-31 20:23:23 +0100

    ENH: analysis__calculate_time_period method create

--- lmr_analyzer/analysis.py
+++ lmr_analyzer/analysis.py
@@ -18,7 +18,33 @@ class analysis:
         -------
         None
         """
-        self.name = name
-        self.routes = routes
+    def __calculate_time_period(self):
+        """Calculate the time period of the analysis. This is calculated by finding the minimum and maximum 
+        time of all routes.
+
+        May be used in the __init__ method.
+
+        Returns
+        -------
+        None
+        """
+
+        # Initialize the time period extremes
+        self.total_start_time = self.routes[0].departure_time
+        self.total_end_time = self.routes[0].departure_time
+
+        # Iterate over all routes to find the correct extremes
+        for route in self.routes:
+            if route.departure_time < self.total_start_time:
+                self.total_start_time = route.departure_time
+            if route.departure_time > self.total_end_time:
+                self.total_end_time = route.departure_time
+
+        # Calculate the time period
+        self.time_period = (self.total_start_time, self.total_end_time)
+        self.time_period_length = self.total_end_time - self.total_start_time
+
+        return None
+
 
         return None
