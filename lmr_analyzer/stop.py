7b3d001cca4b79ffef08dd142c2feaa72a4ef8dd
Gui-FernandesBR <guilherme_fernandes@usp.br>
2022-11-18 07:27:59 +0100

    MAINT: change process_package to properties stop

--- lmr_analyzer/stop.py
+++ lmr_analyzer/stop.py
@@ -1,10 +1,40 @@
 __author__ = "Guilherme Fernandes Alves"
+__email__ = "gf10.alves@gmail.com"
 __license__ = "Mozilla Public License 2.0"
 
 from .package import package
 
 
 class stop:
+    """Class to hold the information of a stop.
+
+    Attributes
+    ----------
+    name : str
+        The name of the stop.
+    location : tuple
+        A tuple containing the coordinates of the stop. The tuple must be
+        in the form (latitude, longitude).
+    location_type : str
+        The location_type of the stop. The location_type must be one of the following:
+        'depot', 'pickup', 'delivery'.
+    time_window : tuple
+        A tuple containing the time window of the stop. The tuple must be
+        in the form (start_time, end_time).
+    packages : list, dict
+        A list or a dictionary containing the packages to be delivered at the stop.
+    packages_list : list
+        A list containing the packages to be delivered at the stop.
+
+    Properties
+    ----------
+    delivery_time : float
+        The delivery time of the stop.
+    ...
+
+    """
+
+    # TODO: Document the class and its methods.
     def __init__(
         self, name, location, location_type, time_window, planned_service_time, packages
     ):
@@ -71,78 +101,195 @@ class stop:
         else:
             raise TypeError("Invalid packages type: must be a list or a dictionary.")
 
-        self.delivery_time = self.time_window[1] - self.time_window[0]
-        self.__process_packages()
+        # The self.status_list will be used by the properties
+        self.status_list = [package.status for package in self.packages_list]
 
         return None
 
-    def __process_packages(self):
-        """Processes the packages of the stop. Counts the number of packages
-        and the total volume of the packages.
+    @property
+    def delivery_time(self):
+        return self.time_window[1] - self.time_window[0]
 
-        Parameters
-        ----------
-        None
+    @property
+    def number_of_delivered_packages(self) -> int:
+        return self.status_list.count("delivered")
+
+    @property
+    def number_of_rejected_packages(self) -> int:
+        return self.status_list.count("rejected")
+
+    @property
+    def number_of_failed_attempted_packages(self) -> int:
+        return self.status_list.count("attempted")
+
+    @property
+    def number_of_to_be_delivered_packages(self) -> int:
+        return self.status_list.count("to-be-delivered")
+
+    @property
+    def number_of_packages(self) -> int:
+        """Returns the number of packages at the stop.
 
         Returns
         -------
-        None
+        int
+            The number of packages at the stop.
         """
         # Count the number of packages
-        self.number_of_packages = len(self.packages)
+        return len(self.packages)
 
-        # Count the number of packages by status
-        status_list = [package.status for package in self.packages_list]
-        self.number_of_delivered_packages = status_list.count("delivered")
-        self.number_of_rejected_packages = status_list.count("rejected")
-        self.number_of_failed_attempted_packages = status_list.count("attempted")
-        self.number_of_to_be_delivered_packages = status_list.count("to-be-delivered")
+    @property
+    def total_volume_of_packages(self) -> float:
+        """Returns the total volume of the packages at the stop.
 
+        Returns
+        -------
+        float
+            The total volume of the packages at the stop.
+        """
         # Count the total volume of the packages
-        volume_list = [package.volume for package in self.packages_list]
-        self.total_volume_of_packages = sum(volume_list)
-        self.average_volume_of_packages = (
-            sum(volume_list) / len(volume_list) if len(volume_list) > 0 else 0
+        return sum([package.volume for package in self.packages_list])
+
+    @property
+    def average_volume_of_packages(self) -> float:
+        """Returns the average volume of the packages at the stop.
+
+        Returns
+        -------
+        float
+            The average volume of the packages at the stop.
+        """
+        # Count the average volume of the packages
+        return (
+            self.total_volume_of_packages / len(self.packages_list)
+            if len(self.packages_list) > 0
+            else 0
         )
 
-        # Count the total volume of the packages by status
-        self.total_volume_of_delivered_packages = [
-            package.volume
-            for package in self.packages_list
-            if package.status == "delivered"
-        ]
-        self.total_volume_of_rejected_packages = [
-            package.volume
-            for package in self.packages_list
-            if package.status == "rejected"
-        ]
-        self.total_volume_of_attempted_packages = [
-            package.volume
-            for package in self.packages_list
-            if package.status == "attempted"
-        ]
-        self.total_volume_of_to_be_delivered_packages = [
-            package.volume
-            for package in self.packages_list
-            if package.status == "to-be-delivered"
-        ]
+    @property
+    def total_volume_of_delivered_packages(self) -> float:
+        """Returns the total volume of the delivered packages at the stop.
 
-        self.total_weight_of_packages = sum(
-            [package.weight for package in self.packages_list]
+        Returns
+        -------
+        float
+            The total volume of the delivered packages at the stop.
+        """
+        # Count the total volume of the delivered packages
+        return sum(
+            [
+                package.volume
+                for package in self.packages_list
+                if package.status == "delivered"
+            ]
         )
-        self.average_weight_of_packages = (
+
+    @property
+    def total_volume_of_rejected_packages(self) -> float:
+        """Returns the total volume of the rejected packages at the stop.
+
+        Returns
+        -------
+        float
+            The total volume of the rejected packages at the stop.
+        """
+        # Count the total volume of the rejected packages
+        return sum(
+            [
+                package.volume
+                for package in self.packages_list
+                if package.status == "rejected"
+            ]
+        )
+
+    @property
+    def total_volume_of_failed_attempted_packages(self) -> float:
+        """Returns the total volume of the packages at the stop.
+
+        Returns
+        -------
+        float
+            The total volume of the packages at the stop.
+        """
+        # Count the total volume of the packages
+        return sum(
+            [
+                package.volume
+                for package in self.packages_list
+                if package.status == "attempted"
+            ]
+        )
+
+    @property
+    def total_volume_of_packages(self) -> float:
+        """Returns the total volume of the packages at the stop.
+
+        Returns
+        -------
+        float
+            The total volume of the packages at the stop.
+        """
+        # Count the total volume of the packages
+        return sum(
+            [
+                package.volume
+                for package in self.packages_list
+                if package.status == "to-be-delivered"
+            ]
+        )
+
+    @property
+    def total_weight_of_packages(self) -> float:
+        """Returns the total weight of the packages at the stop.
+
+        Returns
+        -------
+        float
+            The total weight of the packages at the stop.
+        """
+        # Count the total weight of the packages
+        return sum([package.weight for package in self.packages_list])
+
+    @property
+    def average_weight_of_packages(self) -> float:
+        """Returns the average weight of the packages at the stop.
+
+        Returns
+        -------
+        float
+            The average weight of the packages at the stop.
+        """
+        # Count the average weight of the packages
+        return (
             self.total_weight_of_packages / len(self.packages_list)
             if len(self.packages_list) > 0
             else 0
         )
 
-        self.total_price_of_packages = sum(
-            [package.price for package in self.packages_list]
-        )
-        self.average_price_of_packages = (
+    @property
+    def total_price_of_packages(self) -> float:
+        """Returns the total price of the packages at the stop.
+
+        Returns
+        -------
+        float
+            The total price of the packages at the stop.
+        """
+        # Count the total price of the packages
+        return sum([package.price for package in self.packages_list])
+
+    @property
+    def average_price_of_packages(self) -> float:
+        """Returns the average price of the packages at the stop.
+
+        Returns
+        -------
+        float
+            The average price of the packages at the stop.
+        """
+        # Count the average price of the packages
+        return (
             self.total_price_of_packages / len(self.packages_list)
             if len(self.packages_list) > 0
             else 0
         )
-
-        return None
